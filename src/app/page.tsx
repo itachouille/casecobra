79370668bbcc8cfd83f29bd88d69620430331098
Anthony David <anthony.david34@sfr.fr>
2024-05-06 16:25:36 +0200

    auth with Kinde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Phone from "@/components/Phone";
 import { ArrowRight, Check, Star } from "lucide-react";
 
 export default function Home() {
@@ -82,7 +84,25 @@ export default function Home() {
               </div>
             </div>
           </div>
+          <div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
+            <div className='relative md:max-w-xl'>
+              <img
+                src='/your-image.png'
+                className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
+                alt="a custom case"
+              />
+              <img
+                src='/line.png'
+                className='absolute w-20 -left-6 -bottom-6 select-none'
+                alt="line"
+              />
+              <Phone className='w-64' imgSrc='/testimonials/1.jpg' />
+            </div>
+          </div>
         </MaxWidthWrapper>
+      </section>
+      <section>
+        
       </section>
     </div>
   );
